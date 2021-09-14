8f11c8a68e2d316d449bf2d1175532897163e319
Samnoble118 <samnoble118@icloud.com>
2021-09-14 09:08:09 +0100

    navbar added 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-      </header>
-    </div>
+    <>
+    <Router>
+    <Navbar />
+      <Switch>
+    <Route path="/" exact />
+    </Switch>
+    </Router>
+    </>
+   
+    
   );
 }
 
